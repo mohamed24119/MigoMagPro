99f4c63ca816a21d7d8145167a0c65433748862f
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-09-14 17:07:31 +0200

    Update MigoMagRandomPosts.js

--- MigoMagRandomPosts.js
+++ MigoMagRandomPosts.js
@@ -3,76 +3,72 @@ var l = 0;
 var flag;
 var numofpost = 5;
 function randomposts(json) {
-var total = parseInt(json.feed.openSearch$totalResults.$t, 10);
-for (i = 0; i < numofpost; ) {
-  flag = 0;
-  randarray.length = numofpost;
-  l = Math.floor(Math.random() * total);
-  for (j in randarray) {
-    if (l == randarray[j]) {
-      flag = 1;
+    var total = parseInt(json.feed.openSearch$totalResults.$t, 10);
+    for (i = 0; i < numofpost; ) {
+        flag = 0;
+        randarray.length = numofpost;
+        l = Math.floor(Math.random() * total);
+        for (j in randarray) {
+            if (l == randarray[j]) {
+                flag = 1;
+            }
+        }
+        if (flag == 0 && l != 0) {
+            randarray[i++] = l;
+        }
     }
-  }
-  if (flag == 0 && l != 0) {
-    randarray[i++] = l;
-  }
-}
 
-for (n in randarray) {
-  var p = randarray[n];
-  var entry = json.feed.entry[p - 1];
-  for (k = 0; k < entry.link.length; k++) {
-  if (entry.link[k].rel == "alternate") {
-    var postTitle = entry.title.$t;
-  var postUrl = entry.link[k].href;
- var postID = entry.id.$t.match(/\d+$/)[0];
-var postTime = entry.published.$t;
-var postTimeFormat = postTime.substring(0, 10);
-    
-  document.write('<article class="article-posts" id="post-id-'+ postID +'">');
-    document.write('<div class="box">');
-      document.write('<a class="thumbnail" href="' + postUrl + '">');
-        document.write('<img class="lazy" data-src="' + entry.media$thumbnail.url + '" alt="' + postTitle + '">');
-      document.write('</a>'); // end thumbnail
+    for (n in randarray) {
+        var p = randarray[n];
+        var entry = json.feed.entry[p - 1];
+        for (k = 0; k < entry.link.length; k++) {
+            if (entry.link[k].rel == "alternate") {
+                var postTitle = entry.title.$t;
+                var postUrl = entry.link[k].href;
+                var postID = entry.id.$t.match(/\d+$/)[0];
+                var postTime = entry.published.$t;
+                var postTimeFormat = postTime.substring(0, 10);
 
+                document.write('<article class="article-posts" id="post-id-' + postID + '">');
+                document.write('<div class="box">');
+                document.write('<a class="thumbnail" href="' + postUrl + '">');
+                document.write('<img class="lazy" data-src="' + entry.media$thumbnail.url + '" alt="' + postTitle + '">');
+                document.write("</a>"); // end thumbnail
 
-  document.write('<div class="box-content">');
-  document.write('<header class="article-header">');
-  document.write('<h2 class="post-headding">');
-  document.write('<a class="link" href="' + postUrl + '">');
-  document.write(postTitle);
-  document.write('</a>'); // end link
-  document.write('</h2>'); // end post-headding
-  document.write('</header>'); //end article-header
-    
- document.write('<footer class="article-footer">');
-    document.write('<div class="meta">');
-        document.write('<time class="time" datetime="' + postTime + '" title="' + postTimeFormat + '">');
-        document.write(postTimeFormat);
-        document.write('</time>'); // end time
+                document.write('<div class="box-content">');
+                document.write('<header class="article-header">');
+                document.write('<h2 class="post-headding">');
+                document.write('<a class="link" href="' + postUrl + '">');
+                document.write(postTitle);
+                document.write("</a>"); // end link
+                document.write("</h2>"); // end post-headding
+                document.write("</header>"); //end article-header
 
-  document.write('</div>');
-document.write('</div>');
-    
-document.write('</div>');
-document.write('</div>');
-document.write('</article>');
+                document.write('<footer class="article-footer">');
+                document.write('<div class="meta">');
+                document.write('<time class="time" datetime="' + postTime + '" title="' + postTimeFormat + '">');
+                document.write(postTimeFormat);
+                document.write("</time>"); // end time
+                document.write("</div>");
+                document.write("</footer>");
+
+                document.write("</div>");
+                document.write("</div>");
+                document.write("</article>");
+            }
+        }
     }
-  }
 }
 
-}
-
-
-function gitRandomPosts(url,style) {
-document.write('<div class="featured ' + style + '">');
-document.write('<div class="cat-title">');
-document.write('<a>مشاركات عشوائية</a>');
-document.write('</div>');
-document.write('<div class="articles">');
-document.write('<script src="' + url + '/feeds/posts/default/?alt=json-in-script&start-index=1&max-results=1000&callback=randomposts"/>');
-document.write('</div>');
-document.write('</div>');
+function gitRandomPosts(url, style) {
+    document.write('<div class="featured ' + style + '">');
+    document.write('<div class="cat-title">');
+    document.write("<a>مشاركات عشوائية</a>");
+    document.write("</div>");
+    document.write('<div class="articles">');
+    document.write('<script src="' + url + '/feeds/posts/default/?alt=json-in-script&start-index=1&max-results=1000&callback=randomposts"/>');
+    document.write("</div>");
+    document.write("</div>");
 }
 
 /*
@@ -89,12 +85,12 @@ flag = 0;
 randomPostsArray.length = numRandomPosts;
 l = Math.floor(Math.random() * totalRandomPosts);
 for (j in randomPostsArray) {
-  if (l == randomPostsArray[j]) {
-    flag = 1;
-  }
+if (l == randomPostsArray[j]) {
+flag = 1;
+}
 }
 if (flag == 0 && l != 0) {
-  randomPostsArray[i++] = l;
+randomPostsArray[i++] = l;
 }
 
 }
@@ -105,14 +101,14 @@ for (n in randomPostsArray) {
 var p = randomPostsArray[n];
 var entry = json.feed.entry[p - 1];
 for (k = 0; k < entry.link.length; k++) {
-  if (entry.link[k].rel == "alternate") {
-    document.write('<li>');
-    document.write('<a href=" '+ entry.link[k].href +' ">');
-    document.write('<img src=" ' + entry.media$thumbnail.url +' ">');
-    document.write(entry.title.$t);
-    document.write('</a>');
-    document.write('</li>');
-  }
+if (entry.link[k].rel == "alternate") {
+document.write('<li>');
+document.write('<a href=" '+ entry.link[k].href +' ">');
+document.write('<img src=" ' + entry.media$thumbnail.url +' ">');
+document.write(entry.title.$t);
+document.write('</a>');
+document.write('</li>');
+}
 }
 }
 document.write("</ul>");
