9a5a8daeb7cea57a180ff7aa64b014cf769bdf21
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-12-05 11:02:18 +0200

    Update MigoMagRelatedPostsBlogger.js

--- MigoMagRelatedPostsBlogger.js
+++ MigoMagRelatedPostsBlogger.js
@@ -1,54 +1,51 @@
-var relatedTitles = new Array();
-var relatedPostNum = 0;
-var relatedUrls = new Array();
-var relatedThumbnail = new Array();
+  var relatedTitles = new Array();
+  var relatedPostNum = 0;
+  var relatedUrls = new Array();
+  var relatedThumbnail = new Array();
 
-function RelatedPostslabel(json) {
- for (var i = 0; i < json.feed.entry.length; i++) {
-    var entry = json.feed.entry[i];
-    relatedTitles[relatedPostNum] = entry.title.$t;
-    relatedThumbnail[relatedPostNum] = entry.media$thumbnail.url;
+function related_results_labels(json) {
+   for (var i = 0; i < json.feed.entry.length; i++) {
+      var entry = json.feed.entry[i];
+      relatedTitles[relatedPostNum] = entry.title.$t;
+      relatedThumbnail[relatedPostNum] = entry.media$thumbnail.url;
 
-    for (var k = 0; k < entry.link.length; k++) {
-       if (entry.link[k].rel == 'alternate') {
-          relatedUrls[relatedPostNum] = entry.link[k].href;
-          relatedPostNum++;
-          break;
-       }
+      for (var k = 0; k < entry.link.length; k++) {
+         if (entry.link[k].rel == 'alternate') {
+            relatedUrls[relatedPostNum] = entry.link[k].href;
+            relatedPostNum++;
+            break;
+         }
+      }
+   }
+}
+
+
+
+
+/*
+function contains(a, e) {
+   for(var j = 0; j < a.length; j++) if (a[j]==e) return true;
+return false;
+}*/
+
+
+function printRelatedLabels() {
+  var randomPost = Math.floor((relatedTitles.length - 1) * Math.random());
+  var i = 0;
+  document.write('<ul>');
+  while (i < relatedTitles.length && i < 6) {
+    document.write('<li>');
+    document.write('<a href="' + relatedUrls[randomPost] + '">');
+    document.write('<img src="' + relatedThumbnail[randomPost] + '">');
+    document.write(relatedTitles[randomPost]);
+    document.write('</a>');
+    document.write('<li>');
+    if (randomPost < relatedTitles.length - 1) {
+      randomPost++;
+    } else {
+      randomPost = 0;
     }
- }
-}
-
-
-function PrintRelatedPosts() {
-var randomPost = Math.floor((relatedTitles.length - 1) * Math.random());
-var i = 0;
-while (i < relatedTitles.length && i < 6) {
-document.write('<article class="article-posts">');
-  document.write('<div class="box">');
-  if (showPostThumbnail == true) {
-      document.write('<a class="thumbnail" href="' + relatedUrls[randomPost] + '">');
-      document.write('<img class="lozad" data-src="' + relatedThumbnail[randomPost] + '" alt="' + relatedTitles[randomPost] + '">');
-      document.write('</a>');
+    i++;
   }
-  document.write('<div class="box-content">');
-  document.write('<header class="article-header">');
-  document.write('<h2 class="post-headding">');
-  document.write('<a class="link" href="' + relatedUrls + '">');
-  document.write(relatedTitles[randomPost]);
-  document.write('</a>');
-  document.write('</h2>');
-  document.write('</header>');
-  document.write('</div>');
-  document.write('</div>');
-  document.write('</article>');
-
-  if (randomPost < relatedTitles.length - 1) {
-    randomPost++;
-  } else {
-    randomPost = 0;
-  }
-  i++;
-}
-
+  document.write('</ul>');
 }
