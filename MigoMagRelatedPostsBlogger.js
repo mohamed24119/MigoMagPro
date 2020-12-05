8c661feb7a51e3cfa713e929c19061151ae04936
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-12-05 14:11:37 +0200

    Update MigoMagRelatedPostsBlogger.js

--- MigoMagRelatedPostsBlogger.js
+++ MigoMagRelatedPostsBlogger.js
@@ -8,7 +8,6 @@ function related_results_labels(json) {
 		var entry = json.feed.entry[i];
 		relatedTitles[relatedPostNum] = entry.title.$t;
 		relatedThumbnail[relatedPostNum] = entry.media$thumbnail.url;
-
 		for (var k = 0; k < entry.link.length; k++) {
 			if (entry.link[k].rel == 'alternate') {
 				relatedUrls[relatedPostNum] = entry.link[k].href;
@@ -24,7 +23,8 @@ function printRelatedLabels() {
 	var randomPost = Math.floor((relatedTitles.length - 1) * Math.random());
 	var i = 0;
 	while (i < relatedTitles.length && i < 6) {
-		var printElements = '<div class="box"><a class="thumbnail" href="'+ relatedUrls[randomPost] +'"><img loading="lazy" class="swiper-lazy lazy" data-src="'+ relatedThumbnail[randomPost] +'" alt="'+ relatedTitles[randomPost] +'"></a><div class="box-content"><h4 class="post-headding"><a class="link" href="'+ relatedUrls[randomPost] +'">'+ relatedTitles[randomPost] +'</a></h4></div></div>';
+		var fixThumbnail = relatedThumbnail[randomPost].replace("s72-c", "s1600");
+		var printElements = '<div class="box"><a class="thumbnail" href="'+ relatedUrls[randomPost] +'"><img loading="lazy" class="swiper-lazy lazy" data-src="'+ fixThumbnail +'" alt="'+ relatedTitles[randomPost] +'"></a><div class="box-content"><h4 class="post-headding"><a class="link" href="'+ relatedUrls[randomPost] +'">'+ relatedTitles[randomPost] +'</a></h4></div></div>';
 		document.write('<article class="article-posts swiper-slide">'+ printElements +'</article>');
 		if (randomPost < relatedTitles.length - 1) {
 			randomPost++;
