0f55780fa1411dd3ecf1570072b0466f2772b290
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-12-09 22:24:42 +0200

    Update MigoMagRelatedPostsBlogger.js

--- MigoMagRelatedPostsBlogger.js
+++ MigoMagRelatedPostsBlogger.js
@@ -27,7 +27,7 @@ function printRelatedLabels(maxPosts) {
 	var i = 0;
 	while (i < relatedTitles.length && i < maxPosts) {
 		var fixThumbnail = relatedThumbnail[randomPost].replace("s72-c", "s1600");
-		var printElements = '<div class="box"><a class="thumbnail" href="'+ relatedUrls[randomPost] +'"><img loading="lazy" class="swiper-lazy lazy" data-src="'+ fixThumbnail +'" alt="'+ relatedTitles[randomPost] +'"></a><div class="box-content"><h4 class="post-headding"><a class="link" href="'+ relatedUrls[randomPost] +'">'+ relatedTitles[randomPost] +'</a></h4></div></div>';
+		var printElements = '<div class="box"><a class="thumbnail" href="'+ relatedUrls[randomPost] +'"><img loading="lazy" class="swiper-lazy lazy" data-src="'+ fixThumbnail +'" alt="'+ relatedTitles[randomPost] +'"></a><div class="box-content"><h5 class="post-headding"><a class="link" href="'+ relatedUrls[randomPost] +'">'+ relatedTitles[randomPost] +'</a></h5></div></div>';
 		document.write('<article class="article-posts swiper-slide">'+ printElements +'</article>');
 		if (randomPost < relatedTitles.length - 1) {
 			randomPost++;
